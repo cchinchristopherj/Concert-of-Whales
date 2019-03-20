aa610cc29afde43b07aa56b63538cba9de377c48
Christopher Chin <37155532+cchinchristopherj@users.noreply.github.com>
2019-03-20 07:19:22 -0700

    Update position of ctxOn button

--- index.js
+++ index.js
@@ -1490,7 +1490,7 @@ function setup() {
     // Button to turn on Audio
     let ctx = getAudioContext();
     ctxOn = createButton('Sound On');
-    ctxOn.position(width+20,40);
+    ctxOn.position(width+20,80);
     ctxOn.mousePressed(() => {
         ctx.resume().then(() => {
             ctxOn.hide();
